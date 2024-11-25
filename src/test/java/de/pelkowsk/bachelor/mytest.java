a25ae25e6f63a80dad94bb848d4ec4df1171f448
Pelkowsk <pelkowsk@rhrk.uni-kl.de>
2024-11-25 17:51:21 +0100

    test Tests

---
+++ src/test/java/de/pelkowsk/bachelor/mytest.java
@@ -0,0 +1,14 @@
+package de.pelkowsk.bachelor;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MyTest {
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}
