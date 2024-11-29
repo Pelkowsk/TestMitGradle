02ac2927c97afd40d502b3ed14bb9398cb4a518f
Pelkowsk <pelkowsk@rhrk.uni-kl.de>
2024-11-30 00:11:13 +0100

    test Python Datei 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,37 +1,40 @@
 plugins {
     java
-    id("antlr") // Hinzufügen des ANTLR-Plugins
+    id("antlr") // ANTLR-Plugin hinzufügen
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+    mavenCentral() // Nutze Maven Central für Abhängigkeiten
 }
 
 dependencies {
-
-    antlr("org.antlr:antlr4:4.13.2") // Korrekte Einbindung von ANTLR
+    antlr("org.antlr:antlr4:4.13.2") // ANTLR-Bibliothek
     implementation("org.antlr:antlr4-runtime:4.13.2") // Laufzeit-Bibliothek für ANTLR
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation(platform("org.junit:junit-bom:5.10.0")) // JUnit BOM für Tests
+    testImplementation("org.junit.jupiter:junit-jupiter") // JUnit Jupiter für Tests
 }
 
 tasks.generateGrammarSource {
-    arguments.addAll(listOf("-visitor", "-listener")) // Generiert Visitor und Listener
+    arguments.addAll(listOf("-visitor", "-listener", "-package", "org.example")) // Paketname für generierte Dateien festlegen
     outputDirectory = file("src/main/gen") // Ausgabeort für generierten Code
 }
 
 sourceSets {
     main {
-        java.srcDirs("src/main/java", "src/main/gen") // Generierter Code wird inkludiert
+        java.srcDirs("src/main/java", "src/main/gen") // Inkludiere generierten Code
     }
     test {
-        java.srcDirs("src/test/java")
+        java.srcDirs("src/test/java") // Quellverzeichnis für Tests
     }
 }
 
 tasks.test {
-    useJUnitPlatform()
+    useJUnitPlatform() // JUnit-Plattform verwenden
+}
+
+tasks.clean {
+    delete("src/main/gen") // Lösche den generierten Code beim Bereinigen des Projekts
 }
